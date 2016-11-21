1a178ece1a91c052cdd380f0253f8d32e42ac6f2
stef <stef.verve@gmail.com>
2016-11-21 11:14:49 -0500

    Initial Commit

---
+++ iPhoneImages/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iPhoneImages
+//
+//  Created by Stefan Verveniotis on 2016-11-21.
+//  Copyright © 2016 Stefan Verveniotis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iPhoneImages/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iPhoneImages
+//
+//  Created by Stefan Verveniotis on 2016-11-21.
+//  Copyright © 2016 Stefan Verveniotis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
